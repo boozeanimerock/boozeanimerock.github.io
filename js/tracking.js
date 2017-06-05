01391e5d9f67e6e160febc2f5976f4d50b804726
8dgree <8dgree@gmail.com>
2017-06-04 18:21:50 -0700

    [Tracking] Add Google Analytics

---
+++ js/tracking.js
@@ -0,0 +1,13 @@
+/* ===================================================
+ * Tracking JS
+ * - Contains Google Analytics setup and potentially more trackings
+ * ================================================ */
+
+// Google Analytics
+ (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+ (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+ m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+ })(window,document,'script','https://www.google-analytics.com/analytics.js','ga');
+
+ ga('create', 'UA-99560851-2', 'auto');
+ ga('send', 'pageview');
